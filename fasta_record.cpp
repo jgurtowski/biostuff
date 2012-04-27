800cd5410922f1b52d2d7d9114d05f82debedd53
Tim Douglas <tdoug870@gmail.com>
2012-04-26 20:02:36 -0400

    Fix bugs in 'operator>>'

--- fasta_record.cpp
+++ fasta_record.cpp
@@ -2,6 +2,7 @@
 
 #include <fasta_record.h>
 
+#include <cassert>
 #include <iostream>
 
 namespace fasta {
@@ -24,18 +25,20 @@ std::ostream& Record::print(std::ostream& stream,
 // FREE OPERATORS
 std::istream& operator>>(std::istream& stream, Record& rhs)
 {
-    if (stream.good() && '>' == stream.peek()) {
+    if (stream.good()) {
         std::string line;
         getline(stream, line);
-        rhs.d_name.assign(line.begin() + 1, line.end());
+        if ('>' == line[0]) {
+            rhs.d_name.assign(line.begin() + 1, line.end());
 
-        std::string body;
-        while (stream.good() && '>' != stream.peek()) {
-            getline(stream, line);
-            body += line;
+            std::string body;
+            while (stream && '>' != stream.peek()) {
+                getline(stream, line);
+                body += line;
+            }
+            rhs.d_body.swap(body);
+            stream.clear();  // handle hitting EOF, 'stream' is still good here
         }
-        rhs.d_body.swap(body);
-        stream.clear();
     }
     return stream;
 }
